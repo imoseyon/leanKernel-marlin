b677104832cd20f99b271cc2827a3632045942a1
imoseyon <imoseyon@gmail.com>
2016-12-04 11:06:01 -0800

    lk lite build changes

--- lk.utils/buildlean.sh
+++ lk.utils/buildlean.sh
@@ -1,49 +1,28 @@
 #!/bin/bash
 
-sdir="/lk2/angler"
-udir="/lk2/angler/lk.utils"
-objdir="/lk2/angler_obj"
-device="angler"
-cc="/data/toolchain/latest/bin/aarch64-"
-filename="lk_${device}_m-v${1}.zip"
+sdir="/lk2/build"
+udir="/lk2/build/lk.utils"
+objdir="/lk2/marlin_obj"
+device="marlin"
+cc="/data/toolchain/latest/bin/aarch64-linux-gnu-"
+filename="lk_${device}-v${1}-fastboot.img"
 mkbootimg="/data/mkbootimg_tools2/mkboot"
 
 compile() {
   cd $sdir
-  sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"-leanKernel-${device}-${1}-\"/ $objdir/.config
+  sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"-lklite-${device}-${1}-\"/ $objdir/.config
   make O=$objdir ARCH=arm64 CROSS_COMPILE=$cc -j4
 }
 
-ramdisk() {
-  cd $sdir/lk.ramdisk
+mkboot() {
+  cd $sdir/lk.utils/ramdisk
   chmod 750 init* sbin/adbd* sbin/healthd
-  chmod 644 default* uevent* res/images/charger/*
-  chmod 755 sbin sbin/lkconfig
-  chmod 700 sbin/lk-post-boot.sh
-  chmod 700 sbin/launch_daemonsu.sh
-  chmod 755 res res/images res/images/charger
-  chmod 640 fstab.angler
+  chmod 644 default* uevent*
+  chmod 755 sbin
+  chmod 755 res res/images
+  chmod 640 fstab.marlin
   cd $sdir
-  $mkbootimg $udir /tmp/boot.img
+  $mkbootimg $udir /tmp/$filename
 }
 
-zipit() {
-  cd $udir
-  cp -f /tmp/boot.img zip/
-  cd zip
-  zip -r /tmp/$1 *
-  rm boot.img
-  cd $sdir
-} 
-
-cm_ramdisk() {
-  cd /tmp/angler.cm/ramdisk
-  cp -a * $sdir/lk.ramdisk
-  cd $sdir
-  git checkout lk.ramdisk/init.rc lk.ramdisk/init.angler.rc lk.ramdisk/fstab.angler
-}
-
-[[ $1 =~ "cm" ]] && [[ `git status -s lk.ramdisk` ]] && echo "uncommitted changes in ramdisk!" && exit
-[[ $1 =~ "cm" ]] && cm_ramdisk
-compile $1 && ramdisk && zipit $filename
-[[ $1 =~ "cm" ]] && git checkout HEAD lk.ramdisk && git clean -f lk.ramdisk
+compile $1 && mkboot
